0fcc7e912c3884eff79ceced641364b9d544c743
ElizabethTorre <etr604@gmail.com>
2020-05-26 16:37:07 -0500

    update title project

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
 	
 	},
   head: {
-    title: 'project-vue',
+    title: 'Latin Movie',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
