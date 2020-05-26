6820526ab16b46d59e8f4d71fd76f4ad88b54860
ElizabethTorre <etr604@gmail.com>
2020-05-26 16:16:28 -0500

    init

---
+++ nuxt.config.js
@@ -0,0 +1,74 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+	router: {
+		base: '/Latin_app/'
+	
+	},
+  head: {
+    title: 'project-vue',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  
+  css: [
+		'bootstrap-css-only/css/bootstrap.min.css',
+		'mdbvue/lib/css/mdb.min.css'
+	],
+	/*
+	** Nuxt.js modules
+	*/
+	modules: [
+		// Doc: https://bootstrap-vue.js.org
+		// 'bootstrap-vue/nuxt',
+		'mdbvue/nuxt',
+		'@nuxtjs/axios'
+	],
+
+	mdbvue: {
+		icons: true, // FA5
+		roboto: false, // font Roboto
+		css: true, // MDB CSS
+		bootstrap: false // Bootstrap CSS
+	},
+	axios: {
+		baseURL: "https://www.omdbapi.com/",
+		//credentials: true
+	},
+	/*
+	** Build configuration
+	*/
+	/*
+  ** Build configuration
+  */
+  build: {
+		transpile: [
+			'mdbvue/lib/components'
+		],
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
